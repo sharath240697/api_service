4e0e866b2dedd05c6560a35d8bbf84e599eed33b
sharath240697 <b.sharath240697@gmail.com>
2021-05-23 13:23:55 +1200

    Initialized Spring Boot project

---
+++ src/main/java/com/sharath/exp_pipeline/api_service/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sharath.exp_pipeline.api_service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiServiceApplication.class);
+	}
+
+}
---
+++ src/test/java/com/sharath/exp_pipeline/api_service/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharath.exp_pipeline.api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
